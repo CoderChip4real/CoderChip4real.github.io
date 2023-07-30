892807e3105551982624e396e4cbbd270750ab0f
CoderChip4real <138956423+CoderChip4real@users.noreply.github.com>
2023-07-30 17:07:04 +1200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,1 @@
-alert('Coding rules! Btw videos under construction');
+alert('**NEW** You can now support me on brave browser! Download here : https://brave.com/ And also videos still under construction - CoderChip');
