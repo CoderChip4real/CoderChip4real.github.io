b158826570a146e8e57dd31050a11b4d9ca8f31a
CoderChip4real <138956423+CoderChip4real@users.noreply.github.com>
2023-12-30 20:18:21 +1300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,1 @@
-alert('**MESSAGE** - IMPORTANT - MAJOR WEBSITE UPDATE DELAYED - NEW RELEASE DATE 14th JANUARY');
+alert('**MESSAGE** - IMPORTANT - MAJOR WEBSITE UPDATE DELAYED - NEW RELEASE DATE 14th JANUARY - Try to find the hiddenn link to see a preview of the main page');
