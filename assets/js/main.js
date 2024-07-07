decb9020dff7f9be6803686d62ff82ea71528da6
CoderChip4real <138956423+CoderChip4real@users.noreply.github.com>
2024-07-08 10:38:49 +1200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,1 @@
-alert('**MESSAGE** - UPDATE PART ! OUT LES GOOO!');
+alert('**MESSAGE** - Currently working on part 2 of 3. also i am going to start majpring towards hardware, and linux, but will still do coding.');
